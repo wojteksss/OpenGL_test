8b4fd7b6d49dcae78faa7659ddafba52c3324780
Wojtek <wojtekpilwinski@gmail.com>
2018-07-19 23:48:44 +0200

    Initial Commit

---
+++ OpenGL_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OpenGL_test
+//
+//  Created by Wojtek Pilwinski on 19/07/2018.
+//  Copyright © 2018 wojteksss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
